872bba3aca3c6d545ba77e8f540bc6ba91be886f
Rafael de Oliveira Lima <34964906+rafael654@users.noreply.github.com>
2021-03-28 04:40:27 -0300

    Códigos Disponíveis estudados e desenvolvidos

---
+++ ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+     class AutenticacaoHelper
+    {
+
+        public bool CompararSenhas(string senhaVerdadeira,string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+
+        }
+    }
+}
--- ByteBank.Modelos/Cliente.cs
+++ ByteBank.Modelos/Cliente.cs
@@ -11,5 +11,19 @@ namespace ByteBank.Modelos
         public string Nome { get; set; }
         public string CPF { get; set; }
         public string Profissao { get; set; }
+
+
+        public override bool Equals(Object obj)
+        {
+            Cliente cliente = obj as Cliente;
+            if(cliente==null)
+            {
+                return false;
+
+            }
+            return CPF == cliente.CPF;
+        }
+
+
     }
 }
